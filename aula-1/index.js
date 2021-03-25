9b701e6bbdf4542b07814188186b05b4f6c2e916
Karen Nakahara <karanakahara@gmail.com>
2021-03-25 20:40:53 -0300

    ajustar fontes

--- aula-1/index.js
+++ aula-1/index.js
@@ -1,14 +1,15 @@
-var valorEmDolarTexto = prompt("Qual o valor em dolar que você quer converter?")
+let valorEmDolarTexto = prompt("Qual o valor em dolar que você quer converter?");
 
-var valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+let valorEmDolarNumero = parseFloat(valorEmDolarTexto);
 
+let valorEmReal = valorEmDolarNumero * 5.50;
+let valorEmRealFixado = valorEmReal.toFixed(2);
 
-var valorEmReal = valorEmDolarNumero * 5.50
-var valorEmRealFixado = valorEmReal.toFixed(2)
+alert(valorEmRealFixado);
 
-alert(valorEmRealFixado)
-
-/* Revisão
-variáveis var int - float - string 
-alert - parseInt - parseFloat - prompt
-operações + somar * multiplicar
+/*
+  Revisão
+  variáveis var int - float - string
+  alert - parseInt - parseFloat - prompt
+  operações + somar * multiplicar
+*/
